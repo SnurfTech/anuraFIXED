d5f30a35251eb6fe20eeafab63dd51bb67a1c57f
SnurfTech <138714694+SnurfTech@users.noreply.github.com>
2026-05-10 14:14:46 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 export OLDDIR="$(pwd | sed 's|.*/||')"
 cd ..
 mv $OLDDIR/codespace-basic-setup.sh .
-rm -r $OLDDIR
 git clone https://github.com/mercuryworkshop/anuraos
-mv anuraos $OLDDIR
+mv anuraos/.* $OLDDIR
+mv anuraos/* $OLDDIR
+rmdir anuraos
+rm $OLDDIR/codespace-basic-setup.sh
 mv codespace-basic-setup.sh $OLDDIR
 cd $OLDDIR
