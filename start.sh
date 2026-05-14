730fbf700724c7c449c8892f14df15bf5641fdf2
SnurfTech <138714694+SnurfTech@users.noreply.github.com>
2026-05-13 18:21:49 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ export OLDDIR="$PWD"
 cd ..
 git clone https://github.com/mercuryworkshop/anuraos
 rm anuraos/codespace-basic-setup.sh
-mv $OLDDIR/codespace-basic-setup.sh anuraos
+cp $OLDDIR/codespace-basic-setup.sh anuraos
 cd anuraos
 echo "cd $PWD" >> ~/.bashrc
